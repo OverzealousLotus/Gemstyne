0ced3ec05aa6b46406f3b06b06deeaf7e4c09c50
OverzealousLotus <aminaabdul.9kg55@slmail.me>
2023-03-27 16:52:14 -0400

    Add a few comments.

--- src/main/java/lotus/tutorialmod/TutorialMod.java
+++ src/main/java/lotus/tutorialmod/TutorialMod.java
@@ -6,14 +6,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
+	// My ModID
 	public static final String MOD_ID = "tutorialmod";
 
+	// A simple way to log messages or events.
 	public static final Logger LOGGER = LoggerFactory.getLogger("tutorialmod");
 
+	// Initialization method.
 	@Override
 	public void onInitialize() {
 
-
+		// Usage of LOGGER
 		LOGGER.info("Hello Fabric world!");
 	}
 }
