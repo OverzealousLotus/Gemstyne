1a788b64d6602c4470b1f02bcbb3a638541da9f5
OverzealousLotus <aminaabdul.9kg55@slmail.me>
2023-10-02 02:42:35 -0400

    Fix Morkite Bucket not registering into Gemstyne's creative group.

--- src/main/java/lotus/gemstyne/fluid/GemstyneFluids.java
+++ src/main/java/lotus/gemstyne/fluid/GemstyneFluids.java
@@ -1,5 +1,8 @@
 package lotus.gemstyne.fluid;
+import io.wispforest.owo.itemgroup.OwoItemSettings;
 import lotus.gemstyne.Gemstyne;
+import lotus.gemstyne.util.GemstyneCreativeGroup;
+import lotus.gemstyne.util.GemstyneRegistry;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
@@ -17,16 +20,16 @@ public class GemstyneFluids {
     private GemstyneFluids() {}
     
     public static final FlowableFluid STILL_LIQUID_MORKITE = Registry.register(Registries.FLUID,
-            new Identifier(Gemstyne.MOD_ID, "liquid_morkite"), new LiquidMorkite.Still());
+        new Identifier(Gemstyne.MOD_ID, "liquid_morkite"), new LiquidMorkite.Still());
     public static final FlowableFluid FLOWING_LIQUID_MORKITE = Registry.register(Registries.FLUID,
-            new Identifier(Gemstyne.MOD_ID, "flowing_liquid_morkite"), new LiquidMorkite.Flowing());
+        new Identifier(Gemstyne.MOD_ID, "flowing_liquid_morkite"), new LiquidMorkite.Flowing());
 
     public static final Block LIQUID_MORKITE_BLOCK = Registry.register(Registries.BLOCK, new Identifier(Gemstyne.MOD_ID,
-            "liquid_morkite_block"), new FluidBlock(GemstyneFluids.STILL_LIQUID_MORKITE, FabricBlockSettings.copyOf(Blocks.WATER)
+        "liquid_morkite_block"), new FluidBlock(GemstyneFluids.STILL_LIQUID_MORKITE, FabricBlockSettings.copyOf(Blocks.WATER)
             .replaceable().liquid()));
-    public static final Item LIQUID_MORKITE_BUCKET = Registry.register(Registries.ITEM, new Identifier(Gemstyne.MOD_ID,
-            "liquid_morkite_bucket"), new BucketItem(GemstyneFluids.STILL_LIQUID_MORKITE,
-            new FabricItemSettings().recipeRemainder(Items.BUCKET).maxCount(1)));
+    public static final Item LIQUID_MORKITE_BUCKET = GemstyneRegistry.designateItem("liquid_morkite_bucket",
+        new BucketItem(GemstyneFluids.STILL_LIQUID_MORKITE,
+            new OwoItemSettings().recipeRemainder(Items.BUCKET).maxCount(1).group(GemstyneCreativeGroup.GEMSTYNE)));
 
     public static void registerFluids() {
         Gemstyne.LOGGER.info("Registering Fluid for " + Gemstyne.MOD_ID);
