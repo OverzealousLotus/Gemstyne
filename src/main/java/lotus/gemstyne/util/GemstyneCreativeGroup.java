1b86aea2054565cd58575501ea194f4b2af28acd
OverzealousLotus <aminaabdul.9kg55@slmail.me>
2023-10-07 02:32:10 -0400

    Add tabs & documentation.

--- src/main/java/lotus/gemstyne/util/GemstyneCreativeGroup.java
+++ src/main/java/lotus/gemstyne/util/GemstyneCreativeGroup.java
@@ -3,13 +3,30 @@ package lotus.gemstyne.util;
 import io.wispforest.owo.itemgroup.Icon;
 import io.wispforest.owo.itemgroup.OwoItemGroup;
 import lotus.gemstyne.Gemstyne;
+import lotus.gemstyne.armor.GemstyneArmorItems;
+import lotus.gemstyne.block.GemstyneOreBlocks;
 import lotus.gemstyne.item.spelunking.GemstyneOreItems;
+import io.wispforest.owo.itemgroup.OwoItemSettings;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
 
+/**
+ * Gemstyne's Creative Group is created using <a href="https://docs.wispforest.io/owo/item-groups">Owolib</a>.
+ * Internally, both BlockItems and Items use {@link OwoItemSettings} ands its
+ * {@link OwoItemSettings#group()} method. In this class, the {@link OwoItemGroup} is defined,
+ * then used in various other locations, primarily {@link GemstyneRegistry}.
+ */
 public class GemstyneCreativeGroup {
     private GemstyneCreativeGroup() {}
 
     public static final OwoItemGroup GEMSTYNE = OwoItemGroup.builder(GemstyneRegistry.id("main"),
-            () -> Icon.of(GemstyneOreItems.MOCHITE.crystal())).build();
+        () -> Icon.of(GemstyneOreItems.MOCHITE.crystal())).initializer(group -> {
+            group.addTab(Icon.of(GemstyneOreItems.BUBBLEGEM), "items", TagKey.of(RegistryKeys.ITEM, GemstyneRegistry.id("item_tab")), false);
+            group.addTab(Icon.of(GemstyneOreBlocks.MOCHITE.deepslateOre()), "ore", TagKey.of(RegistryKeys.ITEM, GemstyneRegistry.id("ore_tab")), false);
+            group.addTab(Icon.of(GemstyneOreItems.CRIMONITE.raw()), "metals", TagKey.of(RegistryKeys.ITEM, GemstyneRegistry.id("metals_tab")), false);
+            group.addTab(Icon.of(GemstyneOreItems.FIRE_OPAL), "gems", TagKey.of(RegistryKeys.ITEM, GemstyneRegistry.id("gems_tab")), false);
+            group.addTab(Icon.of(GemstyneArmorItems.RENDFIRE.chestplate), "equipment", TagKey.of(RegistryKeys.ITEM, GemstyneRegistry.id("equipment_tab")), false);
+    }).build();
 
     public static void registerGroups() {
         Gemstyne.LOGGER.info("Registering Gemstyne Group(s)...");
