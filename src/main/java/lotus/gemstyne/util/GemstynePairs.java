94b2fd45ba0f69605e3748b67af62060f71ec10f
OverzealousLotus <aminaabdul.9kg55@slmail.me>
2023-10-07 02:35:44 -0400

    Improve GemstynePairs

---
+++ src/main/java/lotus/gemstyne/util/GemstynePairs.java
@@ -0,0 +1,21 @@
+package lotus.gemstyne.util;
+
+import net.minecraft.block.AmethystBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ToolItem;
+
+public final class GemstynePairs {
+    private GemstynePairs() {}
+
+    /**
+     * Record to hold a {@link Block} linked to its BlockID.
+     * If you don't know what a Java Record is, then think of it as a class, which defines methods automatically.
+     * In this case, we only care about fetching a {@link Block} and its ID; Java Records define getter methods
+     * for its fields. See <a href="https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/lang/Record.html">Java Documentation</a>
+     */
+    public record BlockPair(String blockID, Block block) {}
+    public record ItemPair(String itemID, Item item) {}
+    public record CrystallinePair(String blockID, AmethystBlock block) {}
+    public record ToolPair(String toolID, ToolItem tool) {}
+}
