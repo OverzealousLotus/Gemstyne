b226748273d523a94fe4b61505137b4a4aac0cd0
OverzealousLotus <aminaabdul.9kg55@slmail.me>
2024-08-11 19:45:33 -0400

    Initial 1.21 Update

--- src/main/java/lotus/gemstyne/client/rendering/UraniumArrowEntityRenderer.java
+++ src/main/java/lotus/gemstyne/client/rendering/UraniumArrowEntityRenderer.java
@@ -2,10 +2,13 @@ package lotus.gemstyne.client.rendering;
 
 import lotus.gemstyne.entity.custom.UraniumArrowEntity;
 import lotus.gemstyne.util.GemstyneRegistry;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.ProjectileEntityRenderer;
 import net.minecraft.util.Identifier;
 
+@Environment(EnvType.CLIENT)
 public class UraniumArrowEntityRenderer extends ProjectileEntityRenderer<UraniumArrowEntity> {
     public static final Identifier TEXTURE = GemstyneRegistry.id("textures/entity/projectiles/uranium_arrow.png");
 
--- src/main/java/lotus/gemstyne/item/custom/UraniumArrow.java
+++ src/main/java/lotus/gemstyne/item/custom/UraniumArrow.java
@@ -3,19 +3,36 @@ package lotus.gemstyne.item.custom;
 import lotus.gemstyne.entity.custom.UraniumArrowEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.item.ArrowItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ProjectileItem;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Position;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class UraniumArrow extends ArrowItem {
-    public UraniumArrow(Item.Settings settings) {
-        super(settings);
-    }
+    public UraniumArrow(Item.Settings settings) { super(settings); }
 
     @Override
     public PersistentProjectileEntity createArrow(World world, ItemStack stack, LivingEntity shooter, @Nullable ItemStack shotFrom) {
         return new UraniumArrowEntity(world, shooter, stack, shotFrom);
+    }
+
+    @Override
+    public ProjectileEntity createEntity(World world, Position pos, ItemStack stack, Direction direction) {
+        var entity = new UraniumArrowEntity(world, pos.getX(), pos.getY(), pos.getZ(), stack.copyWithCount(1), null);
+        entity.pickupType = PersistentProjectileEntity.PickupPermission.ALLOWED;
+        return entity;
+    }
+
+    @Override
+    public ProjectileItem.Settings getProjectileSettings() {
+        return new ProjectileItem.Settings.Builder()
+            .power(1.4f)
+            .uncertainty(5.0f)
+            .build();
     }
 }
